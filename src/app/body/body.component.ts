f6364b29e562eccdf9048cd651377b53f3579378
Jason Liszka <jtl1015@sru.edu>
2019-11-11 18:03:32 -0500

    Added Body

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResumeDataService } from '../resume-data.service';
 
 @Component({
   selector: 'app-body',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
-  constructor() { }
+  resumeData: ResumeData;
+
+  constructor(private resumeDataService: ResumeDataService) { }
+
+  getResumeData(): void {
+    this.resumeData = this.resumeDataService.getResumeData();
+  }
 
   ngOnInit() {
+    this.getResumeData();
   }
 
 }
--- src/app/resume-data.service.ts
+++ src/app/resume-data.service.ts
@@ -11,16 +11,13 @@ export class ResumeDataService {
     return RESUME_DATA;
   }
 
-  getText(): string {
-    return "Hello";
-  }
 }
 
 export class ResumeData {
   header: HeaderData;
-  //education: Education;
-  //job: JobExperience;
-  //footer: Footer;
+  education: Education[];
+  jobs: JobExperience[];
+  footer: Footer;
 }
 
 export class HeaderData {
@@ -56,6 +53,60 @@ export const RESUME_DATA: ResumeData = [
       phone: "(724)-877-2370",
       email: "jasonl8446@protonmail.com",
       description: "I am a computer science student who is skilled at using Linux who also has a strong foundation in mathematical thinking. I am seeking to find a job that allows me to apply my technical skills and knowledge. "
+    },
+    education: [
+        {
+          name: "Slippery Rock University, Slippery Rock, PA",
+          description: [
+            'GPA 3.5',
+            'Major: Bachelors in Computer Science',
+            'Minor: Professional Japanese',
+            'Certificate: Japanese Studies',
+            'Expected Graduation: Fall 2019',
+            'Favorite Class: Linux Shell Scripting',
+            'Extracurricular Project: Built a portable DNS spoof device using a Raspberry Pi '
+          ]
+        },
+        {
+          name: "Skills",
+          description: [
+            'Programming/Scripting languages most familiar with: C++, Java, JavaScript, Python, and Bash',
+            'Very familiar with GNU/Linux in Bash shell environment',
+            'Skilled with critical thinking, and complex mathematical thinking',
+            'Experience with Web Development in three.js and angular'
+          ]},
+        {
+          name: " Interests and Passions",
+          description: [
+            'Japanese Club President',
+            'Participated in school visits to teach children about Japanese culture'
+          ]
+        }
+    ],
+    jobs: [
+      {
+        title: "Salesman",
+        date: "July 2018 - August 2019",
+        companyName: "Carried Away Outfitters, Greenville, PA",
+        description: [
+          'Rented kayaks, paddles boards and bikes.',
+          'Sold kayaks, paddles boards and other related accessories.',
+          'Fixed any IT related/computer related issues such as keeping their computers secure, fixing printers, etc.',
+          'Created bike maps for company related events'
+        ]
+      },
+      {
+        title: "Land Surveying Assistant",
+        date: "July 2017 – September 2017",
+        companyName: "Sorg Surveying, Greenville, PA",
+        description: [
+          'Assisted in carrying equipment, setting up equipment, etc over long distances',
+          'Measured dimensions of houses, barns, and other buildings'
+        ]
+      }
+    ],
+    footer: {
+      note: "Created by Jason Liszka 2019"
     }
   }
 ]
